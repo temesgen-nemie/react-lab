2fdb54da19daf32fafa1bf005560d58150fbbff1
Temesgen <temen9020@gmail.com>
2024-12-29 00:31:42 +0300

    added json-mock-api project folder

---
+++ json-mock-api/src/App.jsx
@@ -0,0 +1,15 @@
+import ProductList from "./components/ProductList";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Product list</h1>
+        <ProductList />
+      </div>
+    </>
+  );
+}
+
+export default App;
